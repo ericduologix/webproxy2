1f75c4e9d21965bee119a54060a31313d9507a12
Eric Willingham <ericwillingham@Erics-MacBook-Pro.local>
2017-05-21 18:23:53 -0400

    Updates

--- testing.js
+++ testing.js
@@ -1,1 +1,26 @@
-Testing
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000); // See (†)
+
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//res.write('request successfully proxied!' + '\n' + JSON.stringify(getParameterByName(req.url, true, 2));
+  res.write('request body'+JSON.stringify(getParameterByName("testing", req.url) + '  '+ getParameterByName("test2", req.url)));
+  res.end();
+}).listen(9000);
